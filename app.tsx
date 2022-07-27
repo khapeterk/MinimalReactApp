397b7ead0d11206ae0f280b6092da56fdbbb3b46
khapeterk <khapeterk@gmail.com>
2022-07-26 21:35:57 -0400

    using createRoot, switch to jsx

--- app.tsx
+++
@@ -1,8 +0,0 @@
-import *  as React from 'react'
-import ReactDOM from 'react-dom'
-
-function App() {
-    return <h1>hello world</h1>
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'))
